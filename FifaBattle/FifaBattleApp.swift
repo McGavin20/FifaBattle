a2e7e23c443f722ee8a48440de991ed888668302
Sharma <smuridzo20@gmail.com>
2023-02-01 12:14:19 +0100

    Initial Commit

---
+++ FifaBattle/FifaBattleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FifaBattleApp.swift
+//  FifaBattle
+//
+//  Created by Sharma on 01/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct FifaBattleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
